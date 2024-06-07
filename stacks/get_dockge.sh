a63d2fdb60c63fda8181efafa8d6aaa57dc72e50
Jano Simas <janosimas@gmail.com>
2024-06-07 21:03:45 +0200

    Fix get_dockge and sample env file

--- stacks/get_dockge.sh
+++ stacks/get_dockge.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 set -o nounset -o pipefail -o errexit
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+STACKS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DOCKGE_PORT=5001
-COMPOSE_FILE=compose.yaml
+COMPOSE_FILE="${STACKS_DIR}/compose.yaml"
 
-curl "https://dockge.kuma.pet/compose.yaml?port=${DOCKGE_PORT}&stacksPath=$SCRIPT_DIR" --output "${COMPOSE_FILE}"
+curl "https://dockge.kuma.pet/compose.yaml?port=${DOCKGE_PORT}&stacksPath=$STACKS_DIR" --output "${COMPOSE_FILE}"
 
 sed -i -e 's#./data:/app/data#${SERVICE_DATA}/dockge/data:/app/data#g' ${COMPOSE_FILE}
 
 echo """
+    env_file:
+      - path: ${STACKS_DIR}/.env
     labels:
       - homepage.group=System
       - homepage.name=Dockge
       - homepage.icon=dockge.png
       - homepage.href=http://${INTERNAL_ADDR}:${DOCKGE_PORT}/
-      - homepage.description=Docker compose manager
-""" >> "${COMPOSE_FILE}"
+      - homepage.description=Docker compose manager""" >> "${COMPOSE_FILE}"
 
 if [[ "${SERVICE_DATA}" ]] && grep -Fq '${SERVICE_DATA}/dockge/data:' ${COMPOSE_FILE}; then
     docker compose up -d
